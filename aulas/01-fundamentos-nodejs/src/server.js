9ac89e8fdf5ef3a6297d403e1a6339468cdc751e
Ana Cristina Carvalho Silva <cristinaana180@hotmail.com>
2023-06-09 22:15:49 -0300

    Conceitos de POST e get, utilizando status code

--- aulas/01-fundamentos-nodejs/src/server.js
+++ aulas/01-fundamentos-nodejs/src/server.js
@@ -1,8 +1,27 @@
 
 import http from 'node:http';
 
+const users = [
+
+]
+
 const server = http.createServer((request,response)=> {
-    return response.end('Helo world')
+   const {method,url} = request
+   if (method == 'GET' && url == '/users' ){
+    return response
+    .setHeader('Content-type','application/json') 
+    .end(JSON.stringify(users));
+   }
+
+   else if(method == 'POST' && url == '/users'){
+    users.push({
+        "id":1,
+        "name":"Ana",
+        "email":"Cristinaana180@hotmail.com"
+    })
+    return response.writeHead(201).end()
+   }
+    return response.writeHead(404).end('Not found')
 })
 
 server.listen(3333)
