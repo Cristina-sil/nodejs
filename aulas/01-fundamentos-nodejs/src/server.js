f826aa80dde81b43aca3429a8a5162b0f797af88
Ana Cristina Carvalho Silva <cristinaana180@hotmail.com>
2023-06-13 11:24:13 -0300

    Corpo da requisição em JSON (Stream & Buffers)

--- aulas/01-fundamentos-nodejs/src/server.js
+++ aulas/01-fundamentos-nodejs/src/server.js
@@ -5,8 +5,19 @@ const users = [
 
 ]
 
-const server = http.createServer((request,response)=> {
-   const {method,url} = request
+const server = http.createServer(async(request,response)=> {
+    const {method,url} = request
+    const buffers = []
+  
+    for await(const chunk of request ){
+      buffers.push(chunk)
+    }
+    try {
+        request.body = JSON.parse(Buffer.concat(buffers).toString())
+    } catch{
+        request.body = null
+    }
+
    if (method == 'GET' && url == '/users' ){
     return response
     .setHeader('Content-type','application/json') 
@@ -14,10 +25,11 @@ const server = http.createServer((request,response)=> {
    }
 
    else if(method == 'POST' && url == '/users'){
+    const {name,email} = request.body
     users.push({
-        "id":1,
-        "name":"Ana",
-        "email":"Cristinaana180@hotmail.com"
+        id:1,
+        name,
+        email
     })
     return response.writeHead(201).end()
    }
