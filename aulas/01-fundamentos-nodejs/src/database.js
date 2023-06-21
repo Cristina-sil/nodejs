b610c18af8af77aade777534a55327eac8df6464
Ana Cristina Carvalho Silva <cristinaana180@hotmail.com>
2023-06-20 23:10:56 -0300

    Banco de dados - Persistindo banco de dados

--- aulas/01-fundamentos-nodejs/src/database.js
+++ aulas/01-fundamentos-nodejs/src/database.js
@@ -1,20 +1,40 @@
+import fs from 'node:fs/promises'
 
-export class Database{
+const databasePath = new URL('../db.json', import.meta.url)
+
+export class Database {
     #database = {}
 
-    select (table){
+    constructor() {
+        fs.readFile(databasePath, 'utf8')
+            .then(data => {
+                this.#database = JSON.parse(data)
+            })
+            .catch (() => {
+                this.#persist()
+            })
+    }
+
+    #persist() {
+        fs.writeFile(databasePath, JSON.stringify(this.#database))
+    }
+
+    select(table) {
         const data = this.#database[table] ?? []
 
         return data
-
     }
-    insert (table,data){
-        if(Array.isArray(this.#database[table])){
+
+    insert(table, data) {
+        if (Array.isArray(this.#database[table])) {
             this.#database[table].push(data)
-        }else {
+        } else {
             this.#database[table] = [data]
         }
-        return data 
+
+        this.#persist();   
+        
+        return data;
     }
 
 }
